103ecde6609c9d5a9d275fe2918cb6d7a042db17
amber-alchemist <amber-alchemist-147@proton.me>
2026-07-29 18:28:56 +0400

    Add solution of 'Sort Colors' problem

---
+++ medium/sort-colors/Solution.cs
@@ -0,0 +1,18 @@
+﻿// https://leetcode.com/problems/sort-colors
+// #two_pointers
+public class Solution
+{
+	public void SortColors(int[] nums)
+	{
+		// Invariant: all elements before left are 0, and all elements after right are 2.
+		int left = 0, right = nums.Length - 1;
+		for (int middle = left; middle <= right; ++middle) {
+			for (; middle < right && nums[middle] == 2; --right) {
+				(nums[middle], nums[right]) = (nums[right], nums[middle]);
+			}
+			if (nums[middle] == 0) {
+				(nums[middle], nums[left]) = (nums[left++], 0);
+			}
+		}
+	}
+}
