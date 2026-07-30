9b3a4f10f67841fada6c956b6ece1de6595ae86a
amber-alchemist <amber-alchemist-147@proton.me>
2026-07-30 21:07:21 +0400

    Restore solution of 'Earliest Finish Time for Land and Water Rides II' problem

---
+++ medium/earliest-finish-time-for-land-and-water-rides-ii/Solution.cs
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/earliest-finish-time-for-land-and-water-rides-ii
+// #greedy_algorithm
+public class Solution
+{
+	public int EarliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration)
+	{
+		int n = landStartTime.Length;
+		int m = waterStartTime.Length;
+
+		int earliestFinishTime = int.MaxValue;
+
+		int earliestLandEnd = int.MaxValue;
+		for (int i = 0; i < n; ++i) {
+			earliestLandEnd = Math.Min(earliestLandEnd, landStartTime[i] + landDuration[i]);
+		}
+
+		int earliestWaterEnd = int.MaxValue;
+		for (int j = 0; j < m; ++j) {
+			earliestWaterEnd = Math.Min(earliestWaterEnd, waterStartTime[j] + waterDuration[j]);
+
+			int currentEndTime = Math.Max(earliestLandEnd, waterStartTime[j]) + waterDuration[j];
+			earliestFinishTime = Math.Min(earliestFinishTime, currentEndTime);
+		}
+
+		for (int i = 0; i < n; ++i) {
+			int currentEndTime = Math.Max(earliestWaterEnd, landStartTime[i]) + landDuration[i];
+			earliestFinishTime = Math.Min(earliestFinishTime, currentEndTime);
+		}
+
+		return earliestFinishTime;
+	}
+}
