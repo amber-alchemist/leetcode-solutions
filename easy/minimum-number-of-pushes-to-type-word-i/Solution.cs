f126652fbc9565c0fcf65280e928b37e659292ab
amber-alchemist <amber-alchemist-147@proton.me>
2026-07-30 20:25:55 +0400

    Add solution of 'Minimum Number of Pushes to Type Word I' problem

---
+++ easy/minimum-number-of-pushes-to-type-word-i/Solution.cs
@@ -0,0 +1,16 @@
+﻿// https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i
+// #greedy_algorithm
+public class Solution
+{
+	public int MinimumPushes(string word)
+	{
+		int minimumPushesCount = 0;
+		int setsCount = Math.DivRem(word.Length, 8, out int remainPushes);
+		int pushesPerLetter = 1;
+		for (; pushesPerLetter <= setsCount; ++pushesPerLetter) {
+			minimumPushesCount += 8 * pushesPerLetter;
+		}
+		minimumPushesCount += remainPushes * pushesPerLetter;
+		return minimumPushesCount;
+	}
+}
