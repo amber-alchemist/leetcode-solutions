18024f6de44f766acdf4b11bdfd73090dc060073
amber-alchemist <amber-alchemist-147@proton.me>
2026-07-31 14:41:58 +0400

    Add solution of 'Minimum Number of Pushes to Type Word II' problem

---
+++ medium/minimum-number-of-pushes-to-type-word-ii/Solution.cs
@@ -0,0 +1,27 @@
+﻿// https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii
+// #greedy_algorithm #sorting
+public class Solution
+{
+	public int MinimumPushes(string word)
+	{
+		const int AlphabetSize = 26;
+
+		var lettersFrequencies = new int[AlphabetSize];
+		for (int i = 0; i < word.Length; ++i) {
+			++lettersFrequencies[word[i] - 'a'];
+		}
+		Array.Sort(lettersFrequencies);
+
+		int minimumPushes = 0;
+		int pushesPerLetter = 1;
+		int letterNumber = 0;
+		for (int i = AlphabetSize - 1; i >= 0 && lettersFrequencies[i] > 0; --i) {
+			if (letterNumber++ == 8) {
+				++pushesPerLetter;
+				letterNumber = 1;
+			}
+			minimumPushes += lettersFrequencies[i] * pushesPerLetter;
+		}
+		return minimumPushes;
+	}
+}
