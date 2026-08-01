fe62f56b15a3af2b490f9b09916ca8a00c696811
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-01 12:04:23 +0400

    Add solution of 'Sort Array By Parity II' problem

---
+++ easy/sort-array-by-parity-ii/Solution.cs
@@ -0,0 +1,19 @@
+﻿// https://leetcode.com/problems/sort-array-by-parity-ii
+// #two_pointers
+public class Solution
+{
+	public int[] SortArrayByParityII(int[] nums)
+	{
+		int evenIndex = 0, oddIndex = 1;
+		while (evenIndex <= nums.Length - 2) {
+			if (nums[evenIndex] % 2 == 1) {
+				while (nums[oddIndex] % 2 == 1) {
+					oddIndex += 2;
+				}
+				(nums[evenIndex], nums[oddIndex]) = (nums[oddIndex], nums[evenIndex]);
+			}
+			evenIndex += 2;
+		}
+		return nums;
+	}
+}
