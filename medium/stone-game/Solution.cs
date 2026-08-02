78d142f030370399ac9be50ed484d0a11bc02e0c
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-02 15:24:28 +0400

    Add solution of 'Stone Game' problem

---
+++ medium/stone-game/Solution.cs
@@ -0,0 +1,35 @@
+﻿// https://leetcode.com/problems/stone-game
+// #game_theory #two_pointers
+public class Solution
+{
+	public bool StoneGame(int[] piles)
+	{
+		int aliceScore = 0, bobScore = 0;
+		int begin = 0, end = piles.Length - 1;
+		while (begin < end) {
+			if (piles[begin] == piles[end]) {
+				if (begin - end == 1) {
+					aliceScore += piles[begin++];
+					bobScore += piles[end--];
+				} else {
+					if (piles[begin + 1] > piles[end - 1]) {
+						aliceScore += piles[end--];
+						bobScore += piles[begin++];
+					} else {
+						aliceScore += piles[begin++];
+						bobScore += piles[end--];
+					}
+				}
+			} else {
+				if (piles[begin] > piles[end]) {
+					aliceScore += piles[begin++];
+					bobScore += piles[end--];
+				} else {
+					aliceScore += piles[end--];
+					bobScore += piles[begin++];
+				}
+			}
+		}
+		return aliceScore > bobScore;
+	}
+}
