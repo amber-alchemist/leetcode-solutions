1bdec9715b61a0094291bb1671cfc10ddd0faac5
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-06 20:58:04 +0400

    Add solution of 'Smallest Divisible Digit Product I' problem

---
+++ easy/smallest-divisible-digit-product-i/Solution.cs
@@ -0,0 +1,18 @@
+﻿// https://leetcode.com/problems/smallest-divisible-digit-product-i
+// #number_theory
+public class Solution
+{
+	public int SmallestNumber(int n, int t)
+	{
+		int x = Math.DivRem(n, 10, out int y);
+		int p = x == 0 ? y : x * y;
+		while (p % t != 0) {
+			if (++y == 10) {
+				y = 0;
+				++x;
+			}
+			p = y == 0 ? 0 : p + (x == 0 ? 1 : x);
+		}
+		return 10 * x + y;
+	}
+}
