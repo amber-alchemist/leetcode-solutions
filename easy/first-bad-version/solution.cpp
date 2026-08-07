a11dca2347a4d9bd763081554af02f041000d953
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-08 00:22:19 +0400

    Restore solution of 'First Bad Version' problem

---
+++ easy/first-bad-version/solution.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/first-bad-version
+// #binary_search
+class Solution {
+public:
+	int firstBadVersion(int n) {
+		int left = 1;
+		int right = n;
+		while (left < right) {
+			int middle = left + (right - left) / 2;
+			if (isBadVersion(middle)) {
+				right = middle;
+			}
+			else {
+				left = middle + 1;
+			}
+		}
+		return right;
+	}
+};
