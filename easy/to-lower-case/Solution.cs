1533c814d1ab3b36b8347c8ea17056f346d01c5b
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-08 15:46:12 +0400

    Add solution of 'To Lower Case' problem

---
+++ easy/to-lower-case/Solution.cs
@@ -0,0 +1,19 @@
+﻿// https://leetcode.com/problems/to-lower-case
+// #string
+public class Solution
+{
+	public string ToLowerCase(string s)
+	{
+		var newChars = new char[s.Length];
+		for (int i = 0; i < s.Length; ++i) {
+			if (s[i] >= 'A' && s[i] <= 'Z') {
+				int diff = s[i] - 'A';
+				newChars[i] = (char)('a' + diff);
+			}
+			else {
+				newChars[i] = s[i];
+			}
+		}
+		return new string(newChars);
+	}
+}
