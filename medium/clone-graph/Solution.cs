e6dec1050ae3d6ff830d18defdacf16275d97d66
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-09 15:33:07 +0400

    Add solution of 'Clone Graph' problem

---
+++ medium/clone-graph/Solution.cs
@@ -0,0 +1,25 @@
+﻿// https://leetcode.com/problems/clone-graph
+// #graph_theory #dfs
+public class Solution
+{
+	public Node CloneGraph(Node root)
+	{
+		const int MaxNodesCount = 100;
+
+		if (root == null) {
+			return null;
+		}
+		var clonedNodes = new Node[MaxNodesCount];
+		return DeepClone(root);
+
+		Node DeepClone(Node node)
+		{
+			var clone = clonedNodes[node.val - 1] = new Node(node.val);
+			foreach (var neighbor in node.neighbors) {
+				var neighborClone = clonedNodes[neighbor.val - 1] ??= DeepClone(neighbor);
+				clone.neighbors.Add(neighborClone);
+			}
+			return clone;
+		}
+	}
+}
