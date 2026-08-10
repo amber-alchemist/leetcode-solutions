a42babc438ca6db05ace0692b9571e64996641cb
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-10 15:23:55 +0400

    Add solution of 'Generate Parentheses' problem

---
+++ medium/generate-parentheses/Solution.cs
@@ -0,0 +1,32 @@
+﻿// https://leetcode.com/problems/generate-parentheses
+// #stack #combinatorics #backtracking
+public class Solution
+{
+	public IList<string> GenerateParenthesis(int n)
+	{
+		int length = n + n;
+
+		var parentheses = new List<string>();
+		var stack = new char[length];
+		stack[0] = '(';
+		Generate(n - 1, n, 1);
+		return parentheses;
+
+		void Generate(int leftOpen, int leftClose, int index)
+		{
+			if (leftOpen == 0) {
+				for (int i = index; i < length; ++i) {
+					stack[i] = ')';
+				}
+				parentheses.Add(new string(stack));
+			} else {
+				stack[index] = '(';
+				Generate(leftOpen - 1, leftClose, index + 1);
+				if (leftOpen < leftClose) {
+					stack[index] = ')';
+					Generate(leftOpen, leftClose - 1, index + 1);
+				}
+			}
+		}
+	}
+}
