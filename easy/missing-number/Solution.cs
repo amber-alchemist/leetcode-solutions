34d2b47401a88ae26e127e113d639f066f9d5f04
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-10 21:03:07 +0400

    Restore solution of 'Missing Number' problem

---
+++ easy/missing-number/Solution.cs
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/missing-number
+// #array #math
+public class Solution
+{
+	public int MissingNumber(int[] nums)
+	{
+		int n = nums.Length;
+		int sum = n * (n + 1) / 2;
+		for (int i = 0; i < n; ++i) {
+			sum -= nums[i];
+		}
+		return sum;
+	}
+}
