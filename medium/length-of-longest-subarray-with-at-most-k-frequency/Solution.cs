0ead59106c67e6303b793eed76e2cc198dc4aae6
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-12 15:25:10 +0400

    Add solution of 'Length of Longest Subarray With at Most K Frequency' problem

---
+++ medium/length-of-longest-subarray-with-at-most-k-frequency/Solution.cs
@@ -0,0 +1,29 @@
+﻿// https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency
+// #two_pointers #hash_table
+public class Solution
+{
+	public int MaxSubarrayLength(int[] nums, int k)
+	{
+		int longestGoodSubarrayLength = 0;
+		var frequencies = new Dictionary<int, int> { [nums[0]] = 1 };
+		int left = 0, right = 1;
+		while (right < nums.Length) {
+			if (!frequencies.TryGetValue(nums[right], out int frequence)) {
+				frequence = 0;
+			}
+			frequencies[nums[right]] = ++frequence;
+			if (frequence > k) {
+				longestGoodSubarrayLength = Math.Max(longestGoodSubarrayLength, right - left);
+				while (left < right) {
+					--frequencies[nums[left]];
+					if (nums[left++] == nums[right]) {
+						break;
+					}
+				}
+			}
+			++right;
+		}
+		longestGoodSubarrayLength = Math.Max(longestGoodSubarrayLength, right - left);
+		return longestGoodSubarrayLength;
+	}
+}
