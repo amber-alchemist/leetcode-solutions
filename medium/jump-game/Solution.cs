e5b5ba73ac65254513156bae71190f293d80739b
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-15 13:54:13 +0400

    Add solution of 'Jump Game' problem

---
+++ medium/jump-game/Solution.cs
@@ -0,0 +1,18 @@
+﻿// https://leetcode.com/problems/jump-game
+// #greedy_algorithm
+public class Solution
+{
+	public bool CanJump(int[] nums)
+	{
+		int requiredJumpLength = 0;
+		for (int i = nums.Length - 1; i > 0; --i) {
+			if (nums[i] < requiredJumpLength) {
+				++requiredJumpLength;
+			}
+			else {
+				requiredJumpLength = 1;
+			}
+		}
+		return nums[0] >= requiredJumpLength;
+	}
+}
