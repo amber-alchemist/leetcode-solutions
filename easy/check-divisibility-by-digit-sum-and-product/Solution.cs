54c56c52fc7ae482484a6492b5c473ff2593b63d
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-22 13:29:53 +0400

    Add solution of 'Check Divisibility by Digit Sum and Product' problem

---
+++ easy/check-divisibility-by-digit-sum-and-product/Solution.cs
@@ -0,0 +1,17 @@
+﻿// https://leetcode.com/problems/check-divisibility-by-digit-sum-and-product
+// #math
+public class Solution
+{
+	public bool CheckDivisibility(int n)
+	{
+		int digitSum = 0;
+		int digitProduct = 1;
+		int numberCopy = n;
+		while (numberCopy > 0) {
+			numberCopy = Math.DivRem(numberCopy, 10, out int digit);
+			digitSum += digit;
+			digitProduct *= digit;
+		}
+		return n % (digitSum + digitProduct) == 0;
+	}
+}
