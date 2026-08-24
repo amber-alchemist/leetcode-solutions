c7cb2c5fd885578201386e659953e815e954850e
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-24 22:25:31 +0400

    Add solution of 'Evaluate Reverse Polish Notation' problem

---
+++ medium/evaluate-reverse-polish-notation/Solution.cs
@@ -0,0 +1,28 @@
+﻿// https://leetcode.com/problems/evaluate-reverse-polish-notation
+// #string #stack
+public class Solution
+{
+	public int EvalRPN(string[] tokens)
+	{
+		var stack = new Stack<int>();
+		for (int i = 0; i < tokens.Length; ++i) {
+			if (int.TryParse(tokens[i], out int number)) {
+				stack.Push(number);
+			}
+			else {
+				char @operator = tokens[i][0];
+				int b = stack.Pop();
+				int a = stack.Pop();
+				int r = @operator switch {
+					'+' => a + b,
+					'-' => a - b,
+					'*' => a * b,
+					'/' => a / b,
+					_ => throw new ArgumentException($"Tokens conatin invalid operator: '{@operator}'"),
+				};
+				stack.Push(r);
+			}
+		}
+		return stack.Pop();
+	}
+}
