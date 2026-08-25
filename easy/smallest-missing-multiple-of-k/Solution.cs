4e061bcd286b6fcdc29a3166659d1a5a445fa9b5
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-25 13:04:53 +0400

    Add solution of 'Smallest Missing Multiple of K' problem

---
+++ easy/smallest-missing-multiple-of-k/Solution.cs
@@ -0,0 +1,22 @@
+﻿// https://leetcode.com/problems/smallest-missing-multiple-of-k
+// #hash_table
+public class Solution
+{
+	public int MissingMultiple(int[] nums, int k)
+	{
+		const int MaxValue = 100;
+
+		var set = new HashSet<int>();
+		for (int i = 0; i < nums.Length; ++i) {
+			if (nums[i] % k == 0) {
+				set.Add(nums[i]);
+			}
+		}
+
+		int smallestMissingMultiple = k;
+		while (smallestMissingMultiple <= MaxValue && set.Contains(smallestMissingMultiple)) {
+			smallestMissingMultiple += k;
+		}
+		return smallestMissingMultiple;
+	}
+}
