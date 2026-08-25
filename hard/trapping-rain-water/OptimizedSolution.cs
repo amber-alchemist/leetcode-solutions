4d1d5160b6b4949798c915fcad6e4f075b8d18ae
amber-alchemist <amber-alchemist-147@proton.me>
2026-08-25 20:58:44 +0400

    Add solutions of 'Trapping Rain Water' problem

---
+++ hard/trapping-rain-water/OptimizedSolution.cs
@@ -0,0 +1,24 @@
+﻿// https://leetcode.com/problems/trapping-rain-water
+// #two_pointers
+public class Solution
+{
+	public int Trap(int[] height)
+	{
+		int totalWaterAmount = 0;
+		int left = 0, right = height.Length - 1;
+		int leftMaxHeight = height[left], rightMaxHeight = height[right];
+		while (left < right) {
+			if (leftMaxHeight <= rightMaxHeight) {
+				++left;
+				leftMaxHeight = Math.Max(leftMaxHeight, height[left]);
+				totalWaterAmount += leftMaxHeight - height[left];
+			}
+			else {
+				--right;
+				rightMaxHeight = Math.Max(rightMaxHeight, height[right]);
+				totalWaterAmount += rightMaxHeight - height[right];
+			}
+		}
+		return totalWaterAmount;
+	}
+}
---
+++ hard/trapping-rain-water/Solution.cs
@@ -0,0 +1,48 @@
+﻿// https://leetcode.com/problems/trapping-rain-water
+// #two_pointers
+public class Solution
+{
+	public int Trap(int[] height)
+	{
+		int totalWaterAmount = 0;
+
+		int rightBorder = height.Length - 1;
+		int left = 0;
+		while (left < rightBorder && height[left] <= height[left + 1]) {
+			++left;
+		}
+
+		int preWaterAmount = 0;
+		int right = left + 1;
+		for (; right <= rightBorder; ++right) {
+			if (height[right] >= height[left]) {
+				totalWaterAmount += preWaterAmount;
+				preWaterAmount = 0;
+				left = right;
+			}
+			else {
+				preWaterAmount += height[left] - height[right];
+			}
+		}
+
+		int leftBorder = left;
+		right = rightBorder;
+		while (right > leftBorder && height[right - 1] >= height[right]) {
+			--right;
+		}
+
+		preWaterAmount = 0;
+		left = right - 1;
+		for (; left >= leftBorder; --left) {
+			if (height[left] >= height[right]) {
+				totalWaterAmount += preWaterAmount;
+				preWaterAmount = 0;
+				right = left;
+			}
+			else {
+				preWaterAmount += height[right] - height[left];
+			}
+		}
+		return totalWaterAmount;
+	}
+}
